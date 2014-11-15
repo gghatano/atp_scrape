e51ab593a95960e093d61b03ecd90cbd7f03e2c8
gg_hatano <adad0405@gmail.com>
2014-11-15 17:20:16 +0900

    add tournament grade

--- collectDataTable.bash
+++ collectDataTable.bash
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-for file in `ls ./dataTable `
+for file in `ls ./dataTable`
 do
-  echo $file
 
   title=$(head -n 1 ./dataTable/$file | 
   sed 's/.$//' |
   sed 's/,/ /')
   echo $title
 
+  grade=$(echo $file | cut -d"_" -f 2)
+
+  echo "TOURNAMENT,GRADE,YEAR,MONTH,DAY,ROUND,PLAYER_1,PLAYER_2,WINNER" > winLoseData.dat
+
   cat dataTable/$file | 
   awk '{if(NR>1) print $0}' | 
-  sed "s/^/$title,/"  >> winLoseData
+  sed "s/^/$title,$grade,/"  >> winLoseData.dat
 done
